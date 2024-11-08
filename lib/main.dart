d611c52b31cee7ba73d2c7ffecf091a32d06df50
TheDuss <murtazaal92@gmail.com>
2024-11-08 09:54:33 +0100

    dummy data added , catgriditem was added and new folders

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
-import 'package:recipapp/categories.dart';
+import 'package:recipapp/screens/categories.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
